e4b4d6243601917247b1868872464244351f5d99
gggabo <gabrielgst93@gmail.com>
2020-03-01 21:21:12 -0500

    Initial commit

---
+++ src/main/java/com/apptest/application/views/dashboard/DashboardView.java
@@ -0,0 +1,56 @@
+package com.apptest.application.views.dashboard;
+
+import com.apptest.application.backend.BackendService;
+import com.apptest.application.backend.Employee;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.GridVariant;
+import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
+import com.vaadin.flow.router.AfterNavigationEvent;
+import com.vaadin.flow.router.AfterNavigationObserver;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+import com.apptest.application.views.main.MainView;
+
+@Route(value = "dashboard", layout = MainView.class)
+@RouteAlias(value = "", layout = MainView.class)
+@PageTitle("Dashboard")
+@CssImport("styles/views/dashboard/dashboard-view.css")
+public class DashboardView extends Div implements AfterNavigationObserver {
+
+    private BackendService service;
+    private final Grid<Employee> grid;
+
+    public DashboardView() {
+        service = new BackendService();
+        setId("dashboard-view");
+        grid = new Grid<>();
+        grid.setId("list");
+        grid.addThemeVariants(GridVariant.LUMO_NO_BORDER,
+                GridVariant.LUMO_NO_ROW_BORDERS);
+        grid.setHeightFull();
+        grid.addColumn(new ComponentRenderer<>(employee -> {
+            H3 h3 = new H3(
+                    employee.getLastname() + ", " + employee.getFirstname());
+            Anchor anchor = new Anchor("mailto:" + employee.getEmail(),
+                    employee.getEmail());
+            anchor.getElement().getThemeList().add("font-size-xs");
+            Div div = new Div(h3, anchor);
+            div.addClassName("employee-column");
+            return div;
+        }));
+
+        add(grid);
+    }
+
+    @Override
+    public void afterNavigation(AfterNavigationEvent event) {
+        // Lazy init of the grid items, happens only when we are sure the view will be
+        // shown to the user
+        grid.setItems(service.getEmployees());
+    }
+}
